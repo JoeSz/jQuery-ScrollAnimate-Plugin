0e9c9a2618f3f74253ca093b9361a0ed2c057a8f
Joe <ujbeszel@gmail.com>
2023-04-16 19:16:04 +0200

    Add trigger for resize too

--- jquery.scrollanimate.js
+++ jquery.scrollanimate.js
@@ -82,7 +82,7 @@
         },
         bindEvents: function () {
             var plugin = this;
-            $(window).on('scroll' + '.' + plugin._name, plugin.throttle(plugin.settings.throttle, function () {
+            $(window).on('scroll' + '.' + plugin._name + ' ' + 'resize' + '.' + plugin._name, plugin.throttle(plugin.settings.throttle, function () {
                 plugin.check();
             }));
         },
